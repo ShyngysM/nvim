d3b8fe5ead02a26e3c7f5dcffbb8beabe9a9337a
ShyngysM <tschingis.muratbek@gmail.com>
2025-04-21 23:01:05 +0200

    enabled extra surroundings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- vim.keymap.set("n", "<leader>ctd", ":lua vim.diagnostic.enable(false) <CR>", { desc = "Diagnostic [D]isable" })
--- vim.keymap.set("n", "<leader>cte", ":lua vim.diagnostic.enable(true) <CR>", { desc = "Diagnostic [E]nable" })
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 vim.keymap.set("n", "<leader>ut", ":TransparentToggle <CR>", { desc = "[T]oggle [T]ransparency" })
+-- vim.keymap.set("n", "<leader>e", ":Lexplore <CR>", { desc = "Default netrw" })
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,4 @@
 return {
-  -- disable trouble
+  -- disable tabline
   { "akinsho/bufferline.nvim", enabled = false },
 }
