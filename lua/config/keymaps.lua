6a9cf0eb527ca634c0e7b0471db1c6a7ca20d65e
ShyngysM <tschingis.muratbek@gmail.com>
2025-06-11 19:19:58 +0200

    added toggle term

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,4 +3,6 @@
 -- Add any additional keymaps here
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 vim.keymap.set("n", "<leader>ut", ":TransparentToggle <CR>", { desc = "[T]oggle [T]ransparency" })
+vim.keymap.set('n', '<leader>l', ':lua require("nvterm.terminal").toggle "vertical" <CR>', { desc = 'Toggle terminal vertical' })
+vim.keymap.set('n', '<leader>j', ':lua require("nvterm.terminal").toggle "horizontal" <CR>', { desc = 'Toggle terminal horizontal' })
 -- vim.keymap.set("n", "<leader>e", ":Lexplore <CR>", { desc = "Default netrw" })
